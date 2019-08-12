f8aa8f8f8921b8bb33493d18558a80e6af45b5dd
LolloWollo <54026484+LolloWollo@users.noreply.github.com>
2019-08-12 13:34:35 +0200

    La prima cartella su LolloWollo Prova
    Caricato documenti a caso su questa repository, proverò ora ad accederci.
    NOTA:  account git configurato sul terminale NON è questo, quindi dovrò cambiare configurazione se voglio accedere ad account mio

---
+++ problem.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include "book.hh"
+
+class Student
+{
+  std::string name;
+  unsigned int id;
+  unsigned int age;
+  char gender;
+  std::vector<Book> * books;
+
+  /*Copy-constructor*/
+  Student (const Student & other)
+  {
+    name = other.name;
+    id = other.id;
+    age = other.age;
+    gender = other.gender;
+    books = other.books;
+  }
+
+  /*Assignment operator*/
+  Student operator = (const Student & other)
+  {
+    name = other.name;
+    id = other.id;
+    age = other.age;
+    gender = other.gender;
+    delete books;
+    books = new std::vector<book> (*other.books);
+    return *this;
+  }
+}
